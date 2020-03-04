9689472c69aa49a938cbcb5991377c6474deda6b
israelgoomes <rael_goomes@hotmail.com>
2020-03-04 20:41:28 -0300

    bando de dados h2 para teste e criação automatica da base de dados

--- src/main/java/com/cursopring/CursopringbootApplication.java
+++ src/main/java/com/cursopring/CursopringbootApplication.java
@@ -3,11 +3,14 @@ package com.cursopring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursopring.domain.Categoria;
+
 @SpringBootApplication
 public class CursopringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursopringbootApplication.class, args);
+		
 	}
 
 }

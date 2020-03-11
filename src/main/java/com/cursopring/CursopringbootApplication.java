0e499d694767a63f80076f037fd83648fdb2dc94
israelgoomes <rael_goomes@hotmail.com>
2020-03-10 21:27:53 -0300

    Produto e associação muitos para muitos com categoria

--- src/main/java/com/cursopring/CursopringbootApplication.java
+++ src/main/java/com/cursopring/CursopringbootApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cursopring.dao.CategoriaDAO;
+import com.cursopring.dao.ProdutoDAO;
 import com.cursopring.domain.Categoria;
+import com.cursopring.domain.Produto;
 
 @SpringBootApplication
 public class CursopringbootApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaDAO categoriaDao;
+	
+	@Autowired
+	private ProdutoDAO produtoDao;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursopringbootApplication.class, args);
@@ -27,7 +32,20 @@ public class CursopringbootApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 200.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaDao.saveAll(Arrays.asList(cat1, cat2));
+		produtoDao.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 		
 		
